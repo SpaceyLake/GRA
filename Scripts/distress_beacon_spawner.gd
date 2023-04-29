4f94d92f5d0b7e7f657c894aaeedf4d414e6c96f
SpaceyLake <niklas_hornblad@hotmail.com>
2023-04-29 21:44:39 +0200

    Fixed spawning for beacons

--- Scripts/distress_beacon_spawner.gd
+++ Scripts/distress_beacon_spawner.gd
@@ -2,6 +2,7 @@ extends Node
 
 const MIN_LENGTH_FROM_BASE_SQUARED = 200*200
 const MIN_LENGTH_FROM_DISTRESS_BEACON_SQUARED = 100*100
+const MIN_LENGTH_FROM_SHIP_SQUARED = 100*100
 
 var time_decreasment:int = 1
 var time:int = 15
@@ -13,7 +14,7 @@ var timer:Timer = Timer.new()
 var rnd: RandomNumberGenerator = RandomNumberGenerator.new()
 var path_marker_pool:Array = []
 
-@onready var base_position:Vector2 = get_parent().get_node("Base").get_position()
+@onready var base:Node2D = get_parent().get_node("Base")
 
 func _ready():
 	print(size)
@@ -33,13 +34,19 @@ func _timeout():
 	while not accepted_proposition:
 		proposed_position = Vector2(rnd.randf_range(-size.x * camera.zoom.x/2, size.x * camera.zoom.x/2), rnd.randf_range(-size.y * camera.zoom.y/2, size.y * camera.zoom.y/2))
 		accepted_proposition = true
-		if proposed_position.distance_squared_to(base_position) < MIN_LENGTH_FROM_BASE_SQUARED:
+		if proposed_position.distance_squared_to(base.get_position()) < MIN_LENGTH_FROM_BASE_SQUARED:
 			accepted_proposition = false
 			continue
 		for distress_beacon in distress_beacon_pool.distress_beacons_placed:
 			if distress_beacon.global_position.distance_squared_to(proposed_position) < MIN_LENGTH_FROM_DISTRESS_BEACON_SQUARED:
 				accepted_proposition = false
 				break
+		if accepted_proposition == false:
+			continue
+		for ship in base.deplayed_ships:
+			if ship.global_position.distance_squared_to(proposed_position) < MIN_LENGTH_FROM_SHIP_SQUARED:
+				accepted_proposition = false
+				break
 	var distress_beacon:Node2D = distress_beacon_pool.request_distress_beacon(proposed_position)
-	distress_beacon.set_need(rnd.randi_range(min_need, max_need))
+	distress_beacon.set_needs(rnd.randi_range(min_need, max_need))
 	timer.start(time)
