58101999435110b3a4a9d5ca467d04014ec9693f
SpaceyLake <niklas_hornblad@hotmail.com>
2023-04-29 13:49:20 +0200

    Fixed movement and added delivery function

--- Scripts/delivery_ship.gd
+++ Scripts/delivery_ship.gd
@@ -3,13 +3,14 @@ extends CharacterBody2D
 var howered:bool = false
 var selected:bool = false
 var destinations:Array = []
+var cargo: int = 0
 
 func _ready():
 	mouse_entered.connect(is_howering)
 	mouse_exited.connect(is_not_howering)
 
 func _physics_process(delta):
-	if not destinations.is_empty() and position.distance_to(destinations.front()) < 20:
+	if not destinations.is_empty() and position.distance_to(destinations.front()) < velocity.length()*delta:
 			velocity = Vector2.ZERO
 			destinations.pop_front()
 	if not destinations.is_empty():
@@ -28,6 +29,11 @@ func _input(event):
 			destinations.append(get_global_mouse_position())
 		else:
 			destinations = [get_global_mouse_position()]
+
+func deliver(delivering_cargo:int):
+	var delivered_cargo:int = min(cargo, delivering_cargo)
+	cargo = delivering_cargo
+	return delivering_cargo
 
 func is_howering():
 	howered = true
