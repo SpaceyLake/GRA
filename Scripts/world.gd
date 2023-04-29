a7872ad3a9079151c4f51054cb3b98273cfe6a43
SpaceyLake <niklas_hornblad@hotmail.com>
2023-04-29 17:50:49 +0200

    Created distress beacon pool

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 func _ready():
 	path_marker_pool.node_creation_parent = self
+	distress_beacon_pool.node_creation_parent = self
 
 func _exit_tree():
 	path_marker_pool.node_creation_parent = null
+	distress_beacon_pool.node_creation_parent = null
