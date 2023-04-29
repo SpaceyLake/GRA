eacc8f00616b748682951f827bcf57448e115781
SpaceyLake <niklas_hornblad@hotmail.com>
2023-04-29 17:51:35 +0200

    Extended and bug fixes

--- Scripts/base.gd
+++ Scripts/base.gd
@@ -3,19 +3,18 @@ extends Area2D
 var howered:bool = false
 var selected:bool = false
 const cargo_ship:PackedScene = preload("res://Scenes/delivery_ship.tscn")
-@export var nr_ships = 5
+@export var nr_ships = 1
 var docked_ships:Array = []
 var launched_ship:Node2D
-@onready var world:Node2D = get_parent()
+var selected_ship:Node2D
 var cargo_amount = 5
 
 func _ready():
 	if cargo_ship.can_instantiate():
 		for i in nr_ships:
-			docked_ships.append(cargo_ship.instantiate())
-			world.add_child.call_deferred(docked_ships.back())
-			docked_ships.back().global_position = global_position
-			ship_active(docked_ships.back(), false)
+			var new_ship:Node2D = cargo_ship.instantiate()
+			add_sibling.call_deferred(new_ship)
+			new_ship.global_position = global_position
 	mouse_entered.connect(is_howering)
 	mouse_exited.connect(is_not_howering)
 	body_entered.connect(_on_body_entered)
@@ -27,27 +26,29 @@ func _input(event):
 		else:
 			selected = false
 			launched_ship = null
+			selected_ship = null
 	elif selected and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
-		if (not Input.is_action_pressed("shift") or launched_ship == null) and not docked_ships.is_empty():
+		if selected_ship == null and not docked_ships.is_empty():
+				print(docked_ships)
 				launched_ship = docked_ships.pop_front()
+				print(docked_ships)
 				launched_ship.cargo = cargo_amount
+				launched_ship.add_waypoint(get_global_mouse_position())
+				launched_ship.set_goal_roation()
 				ship_active(launched_ship, true)
-				#launched_ship.add_waypoint(get_global_mouse_position())
-	elif event is InputEventKey and event.is_action_pressed("shift"):
-		if not launched_ship == null:
-			launched_ship.selected = true
+				if (Input.is_action_pressed("shift")):
+					selected_ship = launched_ship
+					selected_ship.select_ship(true)
 	elif event is InputEventKey and event.is_action_released("shift"):
-		if not launched_ship == null:
-			launched_ship.selected = false
-		
+		if not selected_ship == null:
+			selected_ship.select_ship(false)
+			selected_ship = null
 
 func is_howering():
 	howered = true
-	print("Howering")
 
 func is_not_howering():
 	howered = false
-	print("Not howering")
 
 func ship_active(current_ship:Node2D, active:bool):
 	current_ship.visible = active
@@ -57,10 +58,12 @@ func ship_active(current_ship:Node2D, active:bool):
 	current_ship.set_process_internal(active)
 	current_ship.set_process_unhandled_input(active)
 	current_ship.set_process_unhandled_key_input(active)
+	current_ship.restart_particles()
 	if active == false:
 		current_ship.global_position = global_position
 
 func _on_body_entered(body: Node2D):
 	if body != launched_ship:
-		pass
-		#ship_active(body, false)
+		body.select_ship(false)
+		ship_active(body, false)
+		docked_ships.append(body)
