d69e60141e4e5553c6a0aba9dcfecd61bf47023c
Golahirom <golahirom@gmail.com>
2023-04-29 16:52:38 +0200

    Beacon Visuals

--- Scripts/distress_beacon.gd
+++ Scripts/distress_beacon.gd
@@ -2,13 +2,30 @@ extends Area2D
 
 var needs: int = 5
 var timeout: float = 20
-var timer: Timer = Timer.new()
+var timer: float = timeout
 
 func _ready():
-	add_child(timer)
 	body_entered.connect(_on_body_entered)
-	timer.timeout.connect(_timeout)
-	timer.start(timeout)
+
+func _process(delta):
+	timer -= delta
+	if timer > 0:
+		var progress = (timer/timeout)
+		var color = Color("#3fc778")
+		if progress < 0.2:
+			color = Color("#e1534a")
+		elif progress < 0.4:
+			color = Color("#f29546")
+		elif progress < 0.6:
+			color = Color("#ffce00")
+		elif progress < 0.8:
+			color = Color("#bdd156")
+		
+		$Sprite2D.modulate = color
+		$TextureProgressBar.tint_progress = color
+		$TextureProgressBar.value = $TextureProgressBar.max_value*progress
+	else:
+		_timeout()
 
 func _on_body_entered(body: Node2D):
 	needs -= body.deliver(needs)
