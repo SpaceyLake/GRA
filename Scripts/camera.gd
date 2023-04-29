b28d2cd50b5b3aa2fa738d7640d93ab83c85a999
SpaceyLake <niklas_hornblad@hotmail.com>
2023-04-29 20:42:10 +0200

    Moved camera script

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-@export var zoom_decrease = 0.00001
+@export var zoom_decrease = 0.5/900
 @export var min_zoom = 0.5
 @export var start_zoom = 1
 
@@ -13,5 +13,5 @@ func _process(delta):
 		zoom.x = min_zoom
 		zoom.y = min_zoom
 	else:
-		zoom.x -= zoom_decrease
-		zoom.y -= zoom_decrease
+		zoom.x -= zoom_decrease * delta
+		zoom.y -= zoom_decrease * delta
